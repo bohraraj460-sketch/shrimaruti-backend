e65b3390ed74e44b4c0815b7b3af8c421f3ba1d8
bohraraj460-sketch <bohraraj460@gmail.com>
2026-06-18 21:32:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-mongoose.connect('mongodb://127.0.0.1:27017/smf_database')
-  .then(() => console.log("🟢 MongoDB Connected!"))
-  .catch(err => console.log("🔴 MongoDB Error: ", err));
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/shrimaruti";
 
+mongoose.connect(mongoURI)
+  .then(() => console.log("MongoDB Connected Successfully!"))
+  .catch((err) => console.log("Database Connection Error: ", err));
 // ==========================================
 // --- MONGOOSE SCHEMAS (STOCK FIXED) ---
 // ==========================================
